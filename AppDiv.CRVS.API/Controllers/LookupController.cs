6775cd6efd5b7cbc7db1baed3660e197adbf8340
Yayal Abayneh <y.abayneh@appdiv.com>
2023-04-22 13:52:23 +0300

    lookup Controller added

---
+++ AppDiv.CRVS.API/Controllers/LookupController.cs
@@ -0,0 +1,57 @@
+using AppDiv.CRVS.Application.Contracts.DTOs;
+using AppDiv.CRVS.Application.Features.Customers.Command.Create;
+using AppDiv.CRVS.Application.Features.Customers.Command.Delete;
+using AppDiv.CRVS.Application.Features.Customers.Command.Update;
+using AppDiv.CRVS.Application.Features.Customers.Query;
+using AppDiv.CRVS.Application.Features.Lookup.Command.Create;
+using AppDiv.CRVS.Application.Features.Lookup.Query.GetAllLookup;
+using AppDiv.CRVS.Application.Features.Lookup.Query.GetLookupById;
+using AppDiv.CRVS.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppDiv.CRVS.API.Controllers
+{
+    [EnableCors("CorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin,Member,User")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class LookupController : ControllerBase
+    {
+        private readonly ISender _mediator;
+        public LookupController(ISender mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet("GetAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<List<LookupDTO>> Get()
+        {
+            return await _mediator.Send(new GetAllLookupQuery());
+        }
+
+        [HttpPost("Create")]
+        // [ProducesResponseType(StatusCodes.Status200OK)]
+        // [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<ActionResult<LookupDTO>> CreateCustomer([FromBody] CreateLookupCommand command, CancellationToken token)
+        {
+            var result = await _mediator.Send(command, token);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<LookupModel> Get(String id)
+        {
+            return await _mediator.Send(new GetLookupByIdQuery(id));
+        }
+
+
+    }
+}
