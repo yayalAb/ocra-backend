0ad57d5d0b5690e4d21a0f4821e084e3ee9fb42a
t.tsegaye <t.tsegaye@appdiv.com>
2023-04-26 16:44:57 +0300

    fix: certificate request dto for formfile

--- AppDiv.CRVS.Application/Features/CertificateTemplate/Commands/Update/UpdateCertificateTemplateCommand.cs
+++ AppDiv.CRVS.Application/Features/CertificateTemplate/Commands/Update/UpdateCertificateTemplateCommand.cs
@@ -1,11 +1,14 @@
 using AppDiv.CRVS.Application.Contracts.DTOs;
 using AppDiv.CRVS.Application.Contracts.Request;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace AppDiv.CRVS.Application.Features.AddressLookup.Commands.Update
 {
-    public record UpdateCertificateTemplateCommand(UpdateCertificateTemplateRequest CertificateTemplate) : IRequest<object>
+    public record UpdateCertificateTemplateCommand: IRequest<object>
     {
-
+        public Guid Id { get; set; }
+        public string CertificateType { get; set; }
+        public IFormFile SvgFile { get; set; }
     }
 }
