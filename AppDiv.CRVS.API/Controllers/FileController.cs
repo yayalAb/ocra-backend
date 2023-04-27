e10f01c48667c5146a26e279c16882874e773a4b
t.tsegaye <t.tsegaye@appdiv.com>
2023-04-27 17:58:07 +0300

    feat: get file endpoint done

---
+++ AppDiv.CRVS.API/Controllers/FileController.cs
@@ -0,0 +1,28 @@
+
+using AppDiv.CRVS.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AppDiv.CRVS.API.Controllers
+{
+    public class FileController : ApiControllerBase
+    {
+        private readonly IFileService _fileService;
+
+        public FileController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get([FromQuery] Guid id , [FromQuery]string fileType)
+        {
+              var response = _fileService.getFile(id.ToString(),fileType);
+
+            return File(response.file,
+                            "application/octet-stream"
+                            , response.fileName+response.fileExtenion);
+        }
+
+    }
+}
