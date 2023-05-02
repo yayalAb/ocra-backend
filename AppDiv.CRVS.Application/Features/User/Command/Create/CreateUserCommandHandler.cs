fe63b50908803003b96db69ce2d5a6e5b3fa756d
t.tsegaye <t.tsegaye@appdiv.com>
2023-05-02 10:16:15 +0300

    feat: user password send by email

--- AppDiv.CRVS.Application/Features/User/Command/Create/CreateUserCommandHandler.cs
+++ AppDiv.CRVS.Application/Features/User/Command/Create/CreateUserCommandHandler.cs
@@ -4,7 +4,10 @@ using AppDiv.CRVS.Application.Interfaces.Persistence;
 using AppDiv.CRVS.Domain;
 using AppDiv.CRVS.Domain.Entities;
 using AppDiv.CRVS.Domain.Repositories;
+using AppDiv.CRVS.Utility.Config;
+using AppDiv.CRVS.Utility.Services;
 using MediatR;
+using Microsoft.Extensions.Options;
 
 namespace AppDiv.CRVS.Application.Features.User.Command.Create
 {
@@ -13,14 +16,19 @@ namespace AppDiv.CRVS.Application.Features.User.Command.Create
         private readonly IIdentityService _identityService;
         private readonly IGroupRepository _groupRepository;
         private readonly IFileService _fileService;
+        private readonly IMailService _mailService;
+        private readonly SMTPServerConfiguration _config;
+
         public CreateUserCommandHandler(IIdentityService identityService,
                                         IGroupRepository groupRepository,
-                                        IFileService fileService)
+                                        IFileService fileService, IMailService mailService,
+                                        IOptions<SMTPServerConfiguration> config)
         {
             this._groupRepository = groupRepository;
             _identityService = identityService;
             _fileService = fileService;
-
+            _mailService = mailService;
+            _config = config.Value;
         }
         public async Task<CreateUserCommandResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
@@ -90,12 +98,18 @@ namespace AppDiv.CRVS.Application.Features.User.Command.Create
                 };
                 var response = await _identityService.createUser(user);
 
+
+                // save profile image
                 var file = request.UserImage;
                 var folderName = Path.Combine("Resources", "UserProfiles");
                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
                 var fileName = response.id;
                 _fileService.UploadBase64File(file, fileName, pathToSave, FileMode.Create);
 
+                //send password by email    
+                var emailContent = response.password + "  is your default password you can login and change it";
+                var subject = "Welcome to OCRVS";
+                await _mailService.SendAsync(body: emailContent, subject: subject, senderMailAddress: _config.SENDER_ADDRESS, receiver: user.Email, cancellationToken);
             }
             return CreateUserCommadResponse;
         }
