0c28cf3623e447e5f76d389a7928840d41008598
t.tsegaye <t.tsegaye@appdiv.com>
2023-05-10 17:33:59 +0300

    feat: create marriage event validatro added

--- AppDiv.CRVS.Application/Features/Marriage/MarriageEvents/Commands/Create/CreateMarriageEventCommandHandler.cs
+++ AppDiv.CRVS.Application/Features/Marriage/MarriageEvents/Commands/Create/CreateMarriageEventCommandHandler.cs
@@ -1,10 +1,7 @@
-﻿using AppDiv.CRVS.Application.Exceptions;
-using AppDiv.CRVS.Application.Contracts.DTOs;
+﻿
 using AppDiv.CRVS.Application.Mapper;
 using AppDiv.CRVS.Domain.Entities;
-using AppDiv.CRVS.Domain.Repositories;
 using MediatR;
-using ApplicationException = AppDiv.CRVS.Application.Exceptions.ApplicationException;
 using AppDiv.CRVS.Application.Interfaces.Persistence;
 using AppDiv.CRVS.Application.Interfaces;
 
@@ -25,14 +22,32 @@ namespace AppDiv.CRVS.Application.Features.MarriageEvents.Command.Create
         }
         public async Task<CreateMarriageEventCommandResponse> Handle(CreateMarriageEventCommand request, CancellationToken cancellationToken)
         {
+
+
+            var CreateMarriageEventCommandResponse = new CreateMarriageEventCommandResponse();
+
+            var validator = new CreateMarriageEventCommandValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            //Check and log validation errors
+            if (validationResult.Errors.Count > 0)
+            {
+                CreateMarriageEventCommandResponse.Success = false;
+                CreateMarriageEventCommandResponse.ValidationErrors = new List<string>();
+                foreach (var error in validationResult.Errors)
+                    CreateMarriageEventCommandResponse.ValidationErrors.Add(error.ErrorMessage);
+                CreateMarriageEventCommandResponse.Message = CreateMarriageEventCommandResponse.ValidationErrors[0];
+            }
+            if (CreateMarriageEventCommandResponse.Success)
+            {
+
             var marriageEvent = CustomMapper.Mapper.Map<MarriageEvent>(request);
             await _marriageEventRepository.InsertOrUpdateAsync(marriageEvent, cancellationToken);
             await _marriageEventRepository.SaveChangesAsync(cancellationToken);
             _eventDocumentService.saveSupportingDocuments(marriageEvent.Event.EventSupportingDocuments, marriageEvent.Event.PaymentExamption.SupportingDocuments, "Marriage");
 
-            return new CreateMarriageEventCommandResponse { Message = "Marriage Event created Successfully" };
 
-            // var executionStrategy = _marriageEventRepository.Datab.CreateExecutionStrategy();
+            // var executionStrategy = _marriageEventRepository..CreateExecutionStrategy();
             // return await executionStrategy.ExecuteAsync(async () =>
             // {
 
@@ -58,6 +73,9 @@ namespace AppDiv.CRVS.Application.Features.MarriageEvents.Command.Create
 
             // });
 
+            }
+            // return new CreateMarriageEventCommandResponse { Message = "Marriage Event created Successfully" };
+            return CreateMarriageEventCommandResponse;
 
         }
     }
