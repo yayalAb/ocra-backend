94435a6e99a2c202ce1967331a29f947cdef917b
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2023-05-19 17:32:28 +0300

    update Certificate dtos #967

--- AppDiv.CRVS.Application/Contracts/DTOs/CertificatesContent/MarriageCertificateDTO.cs
+++ AppDiv.CRVS.Application/Contracts/DTOs/CertificatesContent/MarriageCertificateDTO.cs
@@ -11,9 +11,7 @@ namespace AppDiv.CRVS.Application.Contracts.DTOs.CertificatesContent
         public string? CertifcateId { get; set; }
         public string? RegBookNo { get; set; }
         public string? BirthCertifcateId { get; set; }
-        // public JObject? BrideFirstName { get; set; }
-        // public JObject? BrideMiddleName { get; set; }
-        // public JObject? BrideLastName { get; set; }
+
         public string? BrideFirstNameOr { get; set; }
         public string? BrideFirstNameAm { get; set; }
         public string? BrideMiddleNameOr { get; set; }
@@ -21,13 +19,9 @@ namespace AppDiv.CRVS.Application.Contracts.DTOs.CertificatesContent
         public string? BrideLastNameOr { get; set; }
         public string? BrideLastNameAm { get; set; }
 
-        // public LookupDTO? BrideNationality { get; set; }
         public string? BrideNationalityOr { get; set; }
         public string? BrideNationalityAm { get; set; }
 
-        // public JObject? GroomFirstName { get; set; }
-        // public JObject? GroomMiddleName { get; set; }
-        // public JObject? GroomLastName { get; set; }
         public string? GroomFirstNameOr { get; set; }
         public string? GroomFirstNameAm { get; set; }
         public string? GroomMiddleNameOr { get; set; }
@@ -35,33 +29,39 @@ namespace AppDiv.CRVS.Application.Contracts.DTOs.CertificatesContent
         public string? GroomLastNameOr { get; set; }
         public string? GroomLastNameAm { get; set; }
 
-        // public LookupDTO? GroomNationality { get; set; }
         public string? GroomNationalityOr { get; set; }
         public string? GroomNationalityAm { get; set; }
 
-        // public DateTime? MarriageDate { get; set; }
         public string? MarriageMonth { get; set; }
         public string? MarriageDay { get; set; }
         public string? MarriageYear { get; set; }
 
-        // public AddressDTO? MarriagePlace { get; set; }
         public string? MarriageAddressOr { get; set; }
         public string? MarriageAddressAm { get; set; }
 
-        // public DateTime? EventRegDate { get; set; }
         public string? EventRegisteredMonth { get; set; }
         public string? EventRegisteredDay { get; set; }
         public string? EventRegisteredYear { get; set; }
 
-        // public DateTime? CertificateGenerationDate { get; set; } = DateTime.Now;
         public string? GeneratedMonth { get; set; }
         public string? GeneratedDay { get; set; }
         public string? GeneratedYear { get; set; }
 
-        // public JObject? CivilRegOfficerFirstName { get; set; }
-        // public JObject? CivilRegOfficerMiddleName { get; set; }
-        // public JObject? CivilRegOfficerLastName { get; set; }
         public string? CivileRegOfficerFullNameOr { get; set; }
         public string? CivileRegOfficerFullNameAm { get; set; }
+
+        //splited Address
+        public string? CountryOr { get; set; }
+        public string? CountryAm { get; set; }
+        public string? RegionOr { get; set; }
+        public string? RegionAm { get; set; }
+        public string? ZoneOr { get; set; }
+        public string? ZoneAm { get; set; }
+        public string? WoredaOr { get; set; }
+        public string? WoredaAm { get; set; }
+        public string? CityOr { get; set; }
+        public string? CityAm { get; set; }
+        public string? KebeleOr { get; set; }
+        public string? KebeleAm { get; set; }
     }
 }
