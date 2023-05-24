c7478bb8f274ef8f2dd0f4f0926bbb05c82f02d2
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2023-05-24 12:11:58 +0300

    add payment request for death #965

--- AppDiv.CRVS.Application/Interfaces/Persistence/IDeathEventRepository.cs
+++ AppDiv.CRVS.Application/Interfaces/Persistence/IDeathEventRepository.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppDiv.CRVS.Application.Interfaces.Persistence.Base;
 using AppDiv.CRVS.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace AppDiv.CRVS.Application.Interfaces.Persistence
 {
     public interface IDeathEventRepository : IBaseRepository<DeathEvent>
     {
+        public DatabaseFacade Database { get; }
+
         Task<DeathEvent> GetIncludedAsync(Guid id);
         Task InsertOrUpdateAsync(DeathEvent entity, CancellationToken cancellationToken);
     }
