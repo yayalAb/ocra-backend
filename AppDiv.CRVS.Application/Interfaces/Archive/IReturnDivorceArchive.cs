52faa9bb15cc8fbe6d1fc78e9d8a49c5a4f528a3
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2023-06-01 17:12:38 +0300

    divorce archive #1011

---
+++ AppDiv.CRVS.Application/Interfaces/Archive/IReturnDivorceArchive.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppDiv.CRVS.Application.Contracts.DTOs.Archive;
+using AppDiv.CRVS.Domain.Entities;
+
+namespace AppDiv.CRVS.Application.Interfaces.Archive
+{
+    public interface IReturnDivorceArchive
+    {
+        public DivorceArchiveDTO GetDivorceArchive(Event divorce, string? BirthCertNo);
+    }
+}
