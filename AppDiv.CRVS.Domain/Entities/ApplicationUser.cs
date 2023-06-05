ecff1d42b947b44d380b6eb33666cd89b38582e1
t.tsegaye <t.tsegaye@appdiv.com>
2023-06-05 18:12:19 +0300

    feat: transaction service

--- AppDiv.CRVS.Domain/Entities/ApplicationUser.cs
+++ AppDiv.CRVS.Domain/Entities/ApplicationUser.cs
@@ -9,12 +9,14 @@ namespace AppDiv.CRVS.Domain
         public string? Otp { get; set; }
         public DateTime? OtpExpiredDate { get; set; }
         public Guid PersonalInfoId { get; set; }
-        public  Guid AddressId { get; set; }
+        public Guid AddressId { get; set; }
         public virtual PersonalInfo PersonalInfo { get; set; }
-        public virtual Address Address { get;set;}
+        public virtual Address Address { get; set; }
         public virtual ICollection<UserGroup> UserGroups { get; set; }
-        public virtual ICollection<CertificateSerialTransfer> SenderCertificateSerialTransfers {get; set; }
-        public virtual ICollection<CertificateSerialTransfer> RecieverCertificateSerialTransfers {get; set; }
+        public virtual ICollection<CertificateSerialTransfer> SenderCertificateSerialTransfers { get; set; }
+        public virtual ICollection<CertificateSerialTransfer> RecieverCertificateSerialTransfers { get; set; }
+        public virtual ICollection<Notification> Notifications { get; set; }
+        public virtual ICollection<Transaction> CivilRegOfficerTransactions { get; set; }
 
     }
 }
