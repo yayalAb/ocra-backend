2cb9be219a1da7a8d4ff79af431bad59a05b8ed6
t.tsegaye <t.tsegaye@appdiv.com>
2023-06-06 18:20:05 +0300

    feat: certificate certification

--- AppDiv.CRVS.Application/Features/Certificates/Query/GetAllPaidCertificateByCivilRegistrarQuery.cs
+++ AppDiv.CRVS.Application/Features/Certificates/Query/GetAllPaidCertificateByCivilRegistrarQuery.cs
@@ -1,6 +1,7 @@
 using AppDiv.CRVS.Application.Common;
 using AppDiv.CRVS.Application.Contracts.DTOs;
 using AppDiv.CRVS.Application.Interfaces.Persistence;
+using AppDiv.CRVS.Domain.Entities;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,7 @@ namespace AppDiv.CRVS.Application.Features.Certificates.Query
         public int? PageCount { set; get; } = 1!;
         public int? PageSize { get; set; } = 10!;
         public Guid CivilRegOfficerId { get; set; }
+        public bool isVerification { get; set; } = false;
     }
 
     public class GetAllPaidCertificateByCivilRegistrarQueryHandler : IRequestHandler<GetAllPaidCertificateByCivilRegistrarQuery, PaginatedList<PaidCertificateDTO>>
@@ -24,19 +26,31 @@ namespace AppDiv.CRVS.Application.Features.Certificates.Query
         }
         public async Task<PaginatedList<PaidCertificateDTO>> Handle(GetAllPaidCertificateByCivilRegistrarQuery request, CancellationToken cancellationToken)
         {
+            var eventByCivilReg = _eventRepository.GetAllQueryableAsync()
+                              .Where(e => e.CivilRegOfficerId == request.CivilRegOfficerId);
+            IQueryable<Event> eventsQueriable;
+            if (request.isVerification)
+            {
+                eventsQueriable = eventByCivilReg.Where(e => e.IsCertified && !e.IsVerified);
+
+            }
+            else
+            {
+                eventsQueriable = eventByCivilReg.Where(e => !e.IsCertified && (e.IsPaid || e.IsExampted));
+            }
+
             return await PaginatedList<PaidCertificateDTO>
                             .CreateAsync(
-                             await _eventRepository.GetAllQueryableAsync()
-                              .Where(e =>e.CivilRegOfficerId == request.CivilRegOfficerId &&(e.IsPaid || e.IsExampted) && !e.IsCertified)
-                              .Include(e => e.EventOwener)
-                              .Select(e => new PaidCertificateDTO{
-                                EventId = e.Id,
-                                CertificateId = e.CertificateId,
-                                EventType = e.EventType,
-                                OwnerFullName = e.EventOwener.FirstNameLang + " "+e.EventOwener.MiddleNameLang + " "+e.EventOwener.LastNameLang,
-                                EventDate = e.EventDate,
-                                EventRegDate = e.EventRegDate,
-                                IsCertified = e.IsCertified
+                              await eventsQueriable.Include(e => e.EventOwener)
+                              .Select(e => new PaidCertificateDTO
+                              {
+                                  EventId = e.Id,
+                                  CertificateId = e.CertificateId,
+                                  EventType = e.EventType,
+                                  OwnerFullName = e.EventOwener.FirstNameLang + " " + e.EventOwener.MiddleNameLang + " " + e.EventOwener.LastNameLang,
+                                  EventDate = e.EventDate,
+                                  EventRegDate = e.EventRegDate,
+                                  IsCertified = e.IsCertified
                               })
                               .ToListAsync()
                                 , request.PageCount ?? 1, request.PageSize ?? 10);
