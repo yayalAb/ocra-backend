375f575e0cb42deaf29206c7b883a0ec2b8ec533
Yayal Abayneh <y.abayneh@appdiv.com>
2023-06-08 18:53:46 +0300

    Add payment Request List endpoint

--- AppDiv.CRVS.Domain/Entities/PaymentRequest.cs
+++ AppDiv.CRVS.Domain/Entities/PaymentRequest.cs
@@ -7,18 +7,17 @@ namespace AppDiv.CRVS.Domain.Entities
 {
     public class PaymentRequest : BaseAuditableEntity
     {
-        public string ReasonStr { get; set; }
-        public Guid EventId { get; set; }
+        public string? ReasonStr { get; set; }
+        public Guid? EventId { get; set; }
+        public Guid? RequestId { get; set; }
         public float Amount { get; set; }
         public bool status { get; set; }
         public Guid PaymentRateId { get; set; }
         public string? PaymentCode { get; set; }
-
         public virtual Event Event { get; set; }
         public virtual PaymentRate PaymentRate { get; set; }
         public virtual Payment Payment { get; set; }
-
-  
+        public virtual Request Request { get; set; }
         [NotMapped]
         public JObject Reason
         {
