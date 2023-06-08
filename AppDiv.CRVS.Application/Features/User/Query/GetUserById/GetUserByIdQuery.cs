9e9abe8a607510180e476dad0935bf1a861557af
t.tsegaye <t.tsegaye@appdiv.com>
2023-06-08 17:27:27 +0300

    fix: user by id contact info include

--- AppDiv.CRVS.Application/Features/User/Query/GetUserById/GetUserByIdQuery.cs
+++ AppDiv.CRVS.Application/Features/User/Query/GetUserById/GetUserByIdQuery.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppDiv.CRVS.Application.Contracts.DTOs;
 using AppDiv.CRVS.Application.Contracts.Request;
+using AppDiv.CRVS.Application.Exceptions;
 using AppDiv.CRVS.Application.Features.Lookups.Query.GetAllUser;
 using AppDiv.CRVS.Application.Interfaces;
 using AppDiv.CRVS.Application.Mapper;
 using AppDiv.CRVS.Domain.Repositories;
 using AppDiv.CRVS.Utility.Contracts;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppDiv.CRVS.Application.Features.User.Query.GetUserById
 {
@@ -29,33 +31,31 @@ namespace AppDiv.CRVS.Application.Features.User.Query.GetUserById
         private readonly IIdentityService _identityService;
         private readonly IUserRepository _userRepository;
 
-        public GetUserByIdQueryHandler(IIdentityService identityService , IUserRepository userRepository)
+        public GetUserByIdQueryHandler(IIdentityService identityService, IUserRepository userRepository)
         {
             _identityService = identityService;
             _userRepository = userRepository;
         }
         public async Task<FetchSingleUserResponseDTO> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
         {
-              var explicitLoadedProperties = new Dictionary<string, Utility.Contracts.NavigationPropertyType>
-                                                {
-                                                    { "UserGroups", NavigationPropertyType.COLLECTION },
-                                                    { "PersonalInfo", NavigationPropertyType.REFERENCE }
-
-                                                  
-                                                };
-            var userData = await _userRepository.GetWithAsync(request.Id,explicitLoadedProperties);
-
-
-
-            return new FetchSingleUserResponseDTO{
-                Id = userData.Id,
-                UserName = userData.UserName,
-                AddressId = userData.AddressId,
-                Email = userData.Email,
-                UserGroups = userData.UserGroups.Select(u => u.Id).ToList(),
-                PersonalInfo = CustomMapper.Mapper.Map<AddPersonalInfoRequest>(userData.PersonalInfo)
-
-            };
+            var userData = await _userRepository.GetAll().Where(u => u.Id == request.Id)
+            .Include(u => u.UserGroups)
+            .Include(u => u.PersonalInfo)
+            .ThenInclude(p => p.ContactInfo)
+            .Select(u => new FetchSingleUserResponseDTO
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                AddressId = u.AddressId,
+                Email = u.Email,
+                UserGroups = u.UserGroups.Select(u => u.Id).ToList(),
+                PersonalInfo = CustomMapper.Mapper.Map<AddPersonalInfoRequest>(u.PersonalInfo)
+            }).FirstOrDefaultAsync();
+            if (userData == null)
+            {
+                throw new NotFoundException($"user with id = {request.Id} is not found");
+            }
+            return userData;
 
         }
     }
