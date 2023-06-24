9220ea2377b56fd2221db83b5e7702f764d05da8
Yayal Abayneh <y.abayneh@appdiv.com>
2023-06-24 11:40:52 +0300

    Fix; Add payment Rate Duplication Check #1087

--- AppDiv.CRVS.Application/Features/PaymentRates/Command/Create/CreatePaymentRateCommandValidator.cs
+++ AppDiv.CRVS.Application/Features/PaymentRates/Command/Create/CreatePaymentRateCommandValidator.cs
@@ -29,19 +29,27 @@ namespace AppDiv.CRVS.Application.Features.PaymentRates.Command.Create
             RuleFor(pr => pr.PaymentRate.Amount)
                 .NotEmpty().WithMessage("{PropertyName} is required.")
                 .NotNull();
-
-            // RuleFor(e => e)
-            //   .MustAsync(phoneNumberUnique)
-            //   .WithMessage("A Customer phoneNumber already exists.");
+            RuleFor(e => e)
+              .MustAsync(PaymentRateDuplicationCheck)
+              .WithMessage("The specified Payment Rate  already exists.");
         }
 
-        //private async Task<bool> phoneNumberUnique(CreateCustomerCommand request, CancellationToken token)
-        //{
-        //    var member = await _repo.GetByIdAsync(request.FirstName);
-        //    if (member == null)
-        //        return true;
-        //    else return false;
-        //}
+        private async Task<bool> PaymentRateDuplicationCheck(CreatePaymentRateCommand request, CancellationToken token)
+        {
+            var member = _repo.GetAll()
+            .Where(x => ((x.PaymentTypeLookup.Id == request.PaymentRate.PaymentTypeLookupId) &&
+            (x.EventLookupId == request.PaymentRate.EventLookupId)) && (x.IsForeign == request.PaymentRate.IsForeign)).FirstOrDefault();
+            if (member == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
 
     }
 }
