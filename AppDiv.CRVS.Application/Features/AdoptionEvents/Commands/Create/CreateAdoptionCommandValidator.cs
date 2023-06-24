5dd783693f309c321c1f8d0d60e8a8e01d7f7085
Yayal Abayneh <y.abayneh@appdiv.com>
2023-06-24 12:59:48 +0300

    fix:Remove Unwanted validation #1096

--- AppDiv.CRVS.Application/Features/AdoptionEvents/Commands/Create/CreateAdoptionCommandValidator.cs
+++ AppDiv.CRVS.Application/Features/AdoptionEvents/Commands/Create/CreateAdoptionCommandValidator.cs
@@ -35,33 +35,33 @@ namespace AppDiv.CRVS.Application.Features.AdoptionEvents.Commands.Create
                .WithMessage("A {PropertyName} must be null on create.");
             RuleFor(e => e.Adoption.AdoptiveFather)
                .Must((e, adoptiveFather) => adoptiveFather != null || e.Adoption.AdoptiveMother != null).WithMessage("both adoptive mother and father cannot be null when registering adoption");
-            When(e => e.Adoption.AdoptiveFather != null, () =>
-            {
-                RuleFor(e => e.Adoption.Event.EventOwener.MiddleName)
-                    .Cascade(CascadeMode.StopOnFirstFailure)
-                    .NotNull()
-                    .NotEmpty()
-                    .Must((e, childMiddleName) => BeEqual(childMiddleName, e.Adoption.AdoptiveFather!.FirstName)).WithMessage("the Child's MiddleName must be same as the Adoptive Father's Firstname");
-                RuleFor(e => e.Adoption.Event.EventOwener.LastName)
-                    .Cascade(CascadeMode.StopOnFirstFailure)
-                    .NotNull()
-                    .NotEmpty()
-                    .Must((e, childLastName) => BeEqual(childLastName, e.Adoption.AdoptiveFather!.LastName)).WithMessage("the Child's lastname must be same as the Adoptive Father's MiddleName");
+            // When(e => e.Adoption.AdoptiveFather != null, () =>
+            // {
+            //     RuleFor(e => e.Adoption.Event.EventOwener.MiddleName)
+            //         .Cascade(CascadeMode.StopOnFirstFailure)
+            //         .NotNull()
+            //         .NotEmpty()
+            //         .Must((e, childMiddleName) => BeEqual(childMiddleName, e.Adoption.AdoptiveFather!.FirstName)).WithMessage("the Child's MiddleName must be same as the Adoptive Father's Firstname");
+            //     RuleFor(e => e.Adoption.Event.EventOwener.LastName)
+            //         .Cascade(CascadeMode.StopOnFirstFailure)
+            //         .NotNull()
+            //         .NotEmpty()
+            //         .Must((e, childLastName) => BeEqual(childLastName, e.Adoption.AdoptiveFather!.LastName)).WithMessage("the Child's lastname must be same as the Adoptive Father's MiddleName");
 
-            });
-            When(e => e.Adoption.AdoptiveFather == null && e.Adoption.AdoptiveMother != null, () =>
-            {
-                RuleFor(e => e.Adoption.Event.EventOwener.MiddleName)
-                    .Cascade(CascadeMode.StopOnFirstFailure)
-                    .NotNull()
-                    .NotEmpty()
-                    .Must((e, childMiddleName) => BeEqual(childMiddleName, e.Adoption.AdoptiveMother!.MiddleName)).WithMessage("the Child's MiddleName must be same as the Adoptive Mother's MiddleName");
-                RuleFor(e => e.Adoption.Event.EventOwener.LastName)
-                    .Cascade(CascadeMode.StopOnFirstFailure)
-                    .NotNull()
-                    .NotEmpty()
-                    .Must((e, childLastName) => BeEqual(childLastName, e.Adoption.AdoptiveMother!.LastName)).WithMessage("the Child's lastname must be same as the Adoptive Mother's Lastname");
-            });
+            // });
+            // When(e => e.Adoption.AdoptiveFather == null && e.Adoption.AdoptiveMother != null, () =>
+            // {
+            //     RuleFor(e => e.Adoption.Event.EventOwener.MiddleName)
+            //         .Cascade(CascadeMode.StopOnFirstFailure)
+            //         .NotNull()
+            //         .NotEmpty()
+            //         .Must((e, childMiddleName) => BeEqual(childMiddleName, e.Adoption.AdoptiveMother!.MiddleName)).WithMessage("the Child's MiddleName must be same as the Adoptive Mother's MiddleName");
+            //     RuleFor(e => e.Adoption.Event.EventOwener.LastName)
+            //         .Cascade(CascadeMode.StopOnFirstFailure)
+            //         .NotNull()
+            //         .NotEmpty()
+            //         .Must((e, childLastName) => BeEqual(childLastName, e.Adoption.AdoptiveMother!.LastName)).WithMessage("the Child's lastname must be same as the Adoptive Mother's Lastname");
+            // });
             RuleFor(p => p.Adoption.ApprovedName.am)
                 .Cascade(CascadeMode.StopOnFirstFailure)
                 .NotNull()
