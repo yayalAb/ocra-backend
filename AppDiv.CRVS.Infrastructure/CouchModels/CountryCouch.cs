316b8ac8888dc7ddcef233daa6ea3a24e16f4bbe
t.tsegaye <t.tsegaye@appdiv.com>
2023-07-03 18:02:15 +0300

    feat: country couch added

--- AppDiv.CRVS.Infrastructure/CouchModels/CountryCouch.cs
+++ AppDiv.CRVS.Infrastructure/CouchModels/CountryCouch.cs
@@ -1,10 +1,9 @@
 using CouchDB.Driver.Types;
 
 namespace AppDiv.CRVS.Infrastructure.CouchModels;
-public class AddressLookupCouch : CouchDocument
+public class CountryCouch : CouchDocument
 {
     public Guid Id { get; set; }
-    public string ParentAddressId { get; set; }
     public string? NameAm { get; set; }
     public string? NameOr { get; set; }
 }
