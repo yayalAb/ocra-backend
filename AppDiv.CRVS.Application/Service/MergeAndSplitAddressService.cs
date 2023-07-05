d770ad912edcc777609d4dd509113c3b9482fe7a
Yayal Abayneh <y.abayneh@appdiv.com>
2023-07-05 10:03:28 +0300

    Address Merge And Split

--- AppDiv.CRVS.Application/Service/MergeAndSplitAddressService.cs
+++ AppDiv.CRVS.Application/Service/MergeAndSplitAddressService.cs
@@ -1,3 +1,4 @@
+using Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,35 +24,36 @@ namespace AppDiv.CRVS.Application.Service
             foreach (var add in AddressList)
             {
 
-                var GetChildAddress = _AddressRepository.GetAll().Where(x => x.ParentAddressId == add.Id && !x.Status).ToList();
-                while (GetChildAddress.Count() > 0)
-                {
-                    GetChildAddress = _AddressRepository.GetAll().Where(x => x.ParentAddressId == add.Id && !x.Status).ToList();
-                    if (GetChildAddress.Count() == 0)
-                    {
-                        foreach (var add1 in AddressList)
-                        {
-                            var Address = new Address
-                            {
-                                Id = Guid.NewGuid(),
-                                AddressName = add1.AddressName,
-                                StatisticCode = add1.StatisticCode,
-                                Code = add1.Code,
-                                AdminLevel = add1.AdminLevel,
-                                AreaTypeLookupId = add1.AreaTypeLookupId,
-                                ParentAddressId = add1.ParentAddressId,
-                                AdminTypeLookupId = add1.AdminTypeLookupId,
-                                OldAddressId = add1.Id
-                            };
-                            var oldAddress = _AddressRepository.GetAll().Where(x => x.Id == add1.Id).FirstOrDefault();
-                            oldAddress.Status = true;
-                            await _AddressRepository.UpdateAsync(oldAddress, x => x.Id);
-                            await _AddressRepository.InsertAsync(Address, cancellationToken);
-                        }
-                    }
-                }
+                // var GetChildAddress = _AddressRepository.GetAll().Where(x => x.ParentAddressId == add.Id && !x.Status).ToList();
+                // while (GetChildAddress.Count() > 0)
+                // {
+                //     GetChildAddress = _AddressRepository.GetAll().Where(x => x.ParentAddressId == add.Id && !x.Status).ToList();
+                //     if (GetChildAddress.Count() == 0)
+                //     {
+                //         foreach (var add1 in AddressList)
+                //         {
+                //             Console.WriteLine("addresss {0}", add1);
+                // var Address = new Address
+                // {
+                //     Id = Guid.NewGuid(),
+                //     AddressName = add1.AddressName,
+                //     StatisticCode = add1.StatisticCode,
+                //     Code = add1.Code,
+                //     AdminLevel = add1.AdminLevel,
+                //     AreaTypeLookupId = add1.AreaTypeLookupId,
+                //     ParentAddressId = add1.ParentAddressId,
+                //     AdminTypeLookupId = add1.AdminTypeLookupId,
+                //     OldAddressId = add1.Id
+                // };
+                // var oldAddress = _AddressRepository.GetAll().Where(x => x.Id == add1.Id).FirstOrDefault();
+                // oldAddress.Status = true;
+                // await _AddressRepository.UpdateAsync(oldAddress, x => x.Id);
+                // await _AddressRepository.InsertAsync(Address, cancellationToken);
+                //         }
+                //     }
+                // }
             }
-            var result = await _AddressRepository.SaveChangesAsync(cancellationToken);
+            // var result = await _AddressRepository.SaveChangesAsync(cancellationToken);
             return "Address Migrated Sucessfully";
         }
     }
