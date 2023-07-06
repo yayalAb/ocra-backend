23ab86dc9642637c27bfb75177b400a6ac78e69f
Yayal Abayneh <y.abayneh@appdiv.com>
2023-07-06 11:15:44 +0300

    Fix:Birth Event Adderss #1060

--- AppDiv.CRVS.Application/Features/BirthEvents/Query/GetBirthEventByIdQuery.cs
+++ AppDiv.CRVS.Application/Features/BirthEvents/Query/GetBirthEventByIdQuery.cs
@@ -1,4 +1,6 @@
 ﻿using AppDiv.CRVS.Application.Contracts.DTOs;
+using AppDiv.CRVS.Application.Exceptions;
+using AppDiv.CRVS.Application.Interfaces;
 using AppDiv.CRVS.Application.Interfaces.Persistence;
 using AppDiv.CRVS.Application.Mapper;
 using AppDiv.CRVS.Domain.Entities;
@@ -27,17 +29,40 @@ namespace AppDiv.CRVS.Application.Features.Customers.Query
     public class GetBirthEventByIdHandler : IRequestHandler<GetBirthEventByIdQuery, BirthEventDTO>
     {
         private readonly IBirthEventRepository _BirthEventRepository;
+        private readonly IDateAndAddressService _AddressService;
 
-        public GetBirthEventByIdHandler(IBirthEventRepository BirthEventRepository)
+        public GetBirthEventByIdHandler(IBirthEventRepository BirthEventRepository, IDateAndAddressService AddressService)
         {
             _BirthEventRepository = BirthEventRepository;
+            _AddressService = AddressService;
         }
         public async Task<BirthEventDTO> Handle(GetBirthEventByIdQuery request, CancellationToken cancellationToken)
         {
 
             var selectedBirthEvent = await _BirthEventRepository.GetWithIncludedAsync(request.Id);
-            return CustomMapper.Mapper.Map<BirthEventDTO>(selectedBirthEvent);
-            // return selectedCustomer;
+            if (selectedBirthEvent == null)
+            {
+                throw new NotFoundException("Birth Event with the Given Id Is Not Found");
+            }
+            var BirthEvent = CustomMapper.Mapper.Map<BirthEventDTO>(selectedBirthEvent);
+            BirthEvent.Father.BirthAddress = await _AddressService.FormatedAddress(BirthEvent?.Father?.BirthAddressId);
+            BirthEvent.Father.ResidentAddress = await _AddressService.FormatedAddress(BirthEvent?.Father?.ResidentAddressId);
+            BirthEvent.Mother.BirthAddress = await _AddressService.FormatedAddress(BirthEvent?.Mother?.BirthAddressId);
+            BirthEvent.Mother.ResidentAddress = await _AddressService.FormatedAddress(BirthEvent?.Mother?.ResidentAddressId);
+            BirthEvent.Event.EventAddress = await _AddressService.FormatedAddress(BirthEvent?.Event?.EventAddressId);
+            BirthEvent.Event.EventOwener.BirthAddress = await _AddressService.FormatedAddress(BirthEvent?.Event.EventOwener?.BirthAddressId);
+            if (BirthEvent?.Event.EventOwener?.ResidentAddressId != null)
+            {
+                BirthEvent.Event.EventOwener.ResidentAddress = await _AddressService.FormatedAddress(BirthEvent?.Event.EventOwener?.ResidentAddressId);
+            }
+            if (BirthEvent.Event.EventRegistrar != null)
+            {
+
+                BirthEvent.Event.EventRegistrar.RegistrarInfo.BirthAddress = await _AddressService.FormatedAddress(BirthEvent?.Event?.EventRegistrar?.RegistrarInfo?.BirthAddressId);
+                BirthEvent.Event.EventRegistrar.RegistrarInfo.ResidentAddress = await _AddressService.FormatedAddress(BirthEvent?.Event?.EventRegistrar?.RegistrarInfo?.ResidentAddressId);
+            }
+
+            return BirthEvent;
         }
     }
 }
