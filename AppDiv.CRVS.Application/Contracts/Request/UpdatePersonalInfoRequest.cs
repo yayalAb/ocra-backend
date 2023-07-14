13f220f0838830089c5328fc5cdc0ec3a38331ac
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2023-07-14 11:51:22 +0300

    fix: marriage update #1085

--- AppDiv.CRVS.Application/Contracts/Request/UpdatePersonalInfoRequest.cs
+++ AppDiv.CRVS.Application/Contracts/Request/UpdatePersonalInfoRequest.cs
@@ -20,7 +20,7 @@ namespace AppDiv.CRVS.Application.Contracts.Request
         public string? NationalId { get; set; }
         public Guid? SexLookupId { get; set; }
         public Guid? PlaceOfBirthLookupId { get; set; }
-        public Guid NationalityLookupId { get; set; }
+        public Guid? NationalityLookupId { get; set; }
         public Guid? TitleLookupId { get; set; }
         public Guid? ReligionLookupId { get; set; }
         public Guid? EducationalStatusLookupId { get; set; }
