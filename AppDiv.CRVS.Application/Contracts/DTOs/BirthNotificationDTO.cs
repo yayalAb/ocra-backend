c0e443f7969e87abed1b5e8956a108cba04c1d4d
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2023-07-16 19:59:45 +0300

    fic: birth notification dto #1069

--- AppDiv.CRVS.Application/Contracts/DTOs/BirthNotificationDTO.cs
+++ AppDiv.CRVS.Application/Contracts/DTOs/BirthNotificationDTO.cs
@@ -9,10 +9,10 @@ namespace AppDiv.CRVS.Application.Contracts.DTOs
     {
         public Guid Id { get; set; }
         // public Guid BirthEventId { get; set; }
-        public Guid DeliveryTypeLookupId { get; set; }
-        public float WeightAtBirth { get; set; }
-        public Guid SkilledProfLookupId { get; set; }
-        public string NotficationSerialNumber { get; set; }
+        public Guid? DeliveryTypeLookupId { get; set; }
+        public float? WeightAtBirth { get; set; }
+        public Guid? SkilledProfLookupId { get; set; }
+        public string? NotficationSerialNumber { get; set; }
         // public virtual LookupDTO DeliveryTypeLookup { get; set; }
         // public virtual LookupDTO SkilledProfLookup { get; set; }
     }
