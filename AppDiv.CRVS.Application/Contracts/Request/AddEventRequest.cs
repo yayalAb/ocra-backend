8b9ab3c80a861e7a71807f4d1ce16be3f8f8f505
Natnael Yirga <115155522+natipro22@users.noreply.github.com>
2023-07-19 16:35:24 +0300

    remove divorce date #1081

--- AppDiv.CRVS.Application/Contracts/Request/AddEventRequest.cs
+++ AppDiv.CRVS.Application/Contracts/Request/AddEventRequest.cs
@@ -8,7 +8,7 @@ namespace AppDiv.CRVS.Application.Contracts.Request
         public Guid? Id { get; set; } = null;
         public string? RegBookNo { get; set; }
         public string? CertificateId { get; set; }
-        public string EventDateEt { get; set; }
+        public string? EventDateEt { get; set; }
         public string EventRegDateEt { get; set; }
         public Guid? EventAddressId { get; set; }
 
