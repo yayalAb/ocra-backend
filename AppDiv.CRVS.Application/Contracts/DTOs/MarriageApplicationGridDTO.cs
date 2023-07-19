f54a855fecc5e9d7625380826b8ea540a5b577fb
Yayal Abayneh <y.abayneh@appdiv.com>
2023-07-19 10:08:35 +0300

    fix: marriage application dto error

--- AppDiv.CRVS.Application/Contracts/DTOs/MarriageApplicationGridDTO.cs
+++ AppDiv.CRVS.Application/Contracts/DTOs/MarriageApplicationGridDTO.cs
@@ -7,7 +7,7 @@ using Application.Common.Mappings;
 
 namespace AppDiv.CRVS.Application.Contracts.DTOs
 {
-    public class MarriageApplicationGridDTO :IMapFrom<MarriageApplication>
+    public class MarriageApplicationGridDTO : IMapFrom<MarriageApplication>
     {
         public Guid? Id { get; set; }
         // public DateTime ApplicationDate { get; set; }
@@ -16,6 +16,11 @@ namespace AppDiv.CRVS.Application.Contracts.DTOs
         // public Guid ApplicationAddressId { get; set; }
         public string? BrideFullName { get; set; }
         public string? GroomFullName { get; set; }
+
+        public MarriageApplicationGridDTO()
+        {
+
+        }
         public MarriageApplicationGridDTO(MarriageApplication? application)
         {
             Id = application?.Id;
