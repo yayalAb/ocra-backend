4e6d628d2bf75683020a5588c5d79a50e73b42bf
t.tsegaye <t.tsegaye@appdiv.com>
2023-07-19 11:56:54 +0300

    fix: user update status fix

--- AppDiv.CRVS.Application/Service/IdentityService.cs
+++ AppDiv.CRVS.Application/Service/IdentityService.cs
@@ -269,7 +269,7 @@ namespace AppDiv.CRVS.Application.Service
             existingUser.PreferedLanguage = user.PreferedLanguage;
             existingUser.AddressId = user.AddressId;
             //if the user was locked and status is updated to true
-            if (user.Status && existingUser.LockoutEnd > DateTime.Now && !existingUser.Status && existingUser.LockoutEnabled)
+            if (user.Status && existingUser.LockoutEnd > DateTime.Now && existingUser.LockoutEnabled)
             {
                 existingUser.LockoutEnd = DateTime.Now;
             }
