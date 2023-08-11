f94c1ecc390f660ccb2753d1b43480305c0da70a
yayal abayneh <y.abayneh@appdiv.com>
2023-08-11 12:25:28 +0300

    birth validation commnet

--- AppDiv.CRVS.Application/Features/BirthEvents/Command/Update/UpdateBirthEventCommandHandler.cs
+++ AppDiv.CRVS.Application/Features/BirthEvents/Command/Update/UpdateBirthEventCommandHandler.cs
@@ -42,18 +42,18 @@ namespace AppDiv.CRVS.Application.Features.BirthEvents.Command.Update
                     var response = new UpdateBirthEventCommandResponse();
                     // Validate the inputs.
                     var validator = new UpdateBirthEventCommandValidator(_eventRepository, _lookupRepository);
-                    // var validationResult = await validator.ValidateAsync(request, cancellationToken);
-                    // //Check and log validation errors
-                    // if (validationResult.Errors.Count > 0)
-                    // {
-                    //     response.Success = false;
-                    //     response.Status = 400;
-                    //     response.ValidationErrors = new List<string>();
-                    //     foreach (var error in validationResult.Errors)
-                    //         response.ValidationErrors.Add(error.ErrorMessage);
-                    //     response.Message = response.ValidationErrors[0];
+                    var validationResult = await validator.ValidateAsync(request, cancellationToken);
+                    //Check and log validation errors
+                    if (validationResult.Errors.Count > 0)
+                    {
+                        response.Success = false;
+                        response.Status = 400;
+                        response.ValidationErrors = new List<string>();
+                        foreach (var error in validationResult.Errors)
+                            response.ValidationErrors.Add(error.ErrorMessage);
+                        response.Message = response.ValidationErrors[0];
 
-                    // }
+                    }
                     if (true)
                     {
                         var SelectedEvent= _eventRepository.GetAll()
