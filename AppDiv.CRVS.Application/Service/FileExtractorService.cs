5c5699a76b4a3ef8bec91119ebeee8cb9d4c887d
t.tsegaye <t.tsegaye@appdiv.com>
2023-08-14 18:18:31 +0300

    fix: file extension validation fix

--- AppDiv.CRVS.Application/Service/FileExtractorService.cs
+++ AppDiv.CRVS.Application/Service/FileExtractorService.cs
@@ -65,6 +65,50 @@ namespace AppDiv.CRVS.Application.Service
             {"application/vnd.ms-excel.sheet.binary.macroEnabled.12", "xlsb"},
             {"application/zip", "zip"}
             };
+        private static readonly Dictionary<string, byte[]> fileMagicNumbers = new Dictionary<string, byte[]>
+        {
+            { "bmp", new byte[] { 0x42, 0x4D } },
+            { "cgm", new byte[] { 0x00, 0x01, 0x00, 0x00, 0x48, 0x0A, 0x00, 0x00 } },
+            { "djv", new byte[] { 0x41, 0x54, 0x26, 0x53 } },
+            { "doc", new byte[] { 0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1 } },
+            { "docx", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "dotx", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "docm", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "dotm", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "gif", new byte[] { 0x47, 0x49, 0x46, 0x38, 0x39, 0x61 } },
+            { "ief", new byte[] { 0x42, 0x5A, 0x68 } },
+            { "iges", new byte[] { 0x53, 0x50, 0x41, 0x43, 0x45, 0x20, 0x49, 0x47, 0x45, 0x53 } },
+            { "jp2", new byte[] { 0x00, 0x00, 0x00, 0x0C, 0x6A, 0x50, 0x20, 0x20, 0x0D, 0x0A, 0x87, 0x0A } },
+            { "jpeg", new byte[] { 0xFF, 0xD8, 0xFF } },
+            { "png", new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A } },
+            { "svg", new byte[] { 0x3C, 0x3F, 0x78, 0x6D, 0x6C, 0x20, 0x76, 0x65 } },
+            { "tiff", new byte[] { 0x49, 0x49, 0x2A, 0x00 } },
+            { "wbmp", new byte[] { 0x00, 0x00, 0x00, 0x0C } },
+            { "pdf", new byte[] { 0x25, 0x50, 0x44, 0x46, 0x2D } },
+            { "pct", new byte[] { 0x00, 0x11, 0xA7, 0x00 } },
+            { "pnm", new byte[] { 0x50, 0x36 } },
+            { "pnt", new byte[] { 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00 } },
+            { "ppm", new byte[] { 0x50, 0x36 } },
+            { "ppt", new byte[] { 0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1 } },
+            { "pptx", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "potx", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "ppsx", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "ppam", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "pptm", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "potm", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "ppsm", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "qti", new byte[] { 0x00, 0x00, 0x00, 0x01 } },
+            { "ps", new byte[] { 0x25, 0x21, 0x50, 0x53, 0x2D } },
+            { "rm", new byte[] { 0x2E, 0x52, 0x4D, 0x46 } },
+            { "xls", new byte[] { 0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1 } },
+            { "xlsx", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "xltx", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "xlsm", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "xltm", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "xlam", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "xlsb", new byte[] { 0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00 } },
+            { "zip", new byte[] { 0x50, 0x4B, 0x03, 0x04 } }
+        };
 
 
         public async Task<string[]> ExtractFile(IFormFile file)
@@ -131,18 +175,7 @@ namespace AppDiv.CRVS.Application.Service
         {
             try
             {
-                string splitted = base64String.Substring(base64String.IndexOf(',') + 1);
-                byte[] byteArray = Convert.FromBase64String(splitted);
-
-
-                // Check if the byte array is not empty
-                if (byteArray == null || byteArray.Length == 0)
-                {
-                    throw new ArgumentException("Byte array is empty or null.");
-                }
-
-                // Get the file extension based on the base64 metadata
-                return GetExtensionFromBase64Metadata(base64String);
+                return GetExtensionFromByteSequence(base64String);
             }
             catch (System.Exception)
             {
@@ -185,10 +218,34 @@ namespace AppDiv.CRVS.Application.Service
 
         private static string? GetExtensionFromMimeType(string mimeType)
         {
+
             if (MIMETypesDictionary.ContainsKey(mimeType))
             {
                 return "." + MIMETypesDictionary[mimeType];
             }
+            return null;
+        }
+        public static string? GetExtensionFromByteSequence(string base64String)
+        {
+            if (!HelperService.IsBase64String(base64String))
+            {
+                return null;
+            }
+            base64String = base64String.Substring(base64String.IndexOf(',') + 1);
+            byte[] fileBytes = Convert.FromBase64String(base64String);
+            foreach (var magicNumber in fileMagicNumbers)
+            {
+                if (ByteArrayStartsWith(fileBytes, magicNumber.Value))
+                {
+                    return magicNumber.Key;
+                }
+            }
+            // string[] parts = base64String.Split(',');
+            // if (parts.Length >= 2 && parts[0].StartsWith("data:") && parts[1].Contains("/"))
+            // {
+            //     string mimeType = parts[0].Split(';')[0].Split(":")[1].Trim();
+            //     return mimeType;
+            // }
             return null;
         }
         public static string? GetMimeType(string base64String)
@@ -199,7 +256,25 @@ namespace AppDiv.CRVS.Application.Service
                 string mimeType = parts[0].Split(';')[0].Split(":")[1].Trim();
                 return mimeType;
             }
-            return null;
+            return GetExtensionFromByteSequence(base64String);
+
+        }
+        private static bool ByteArrayStartsWith(byte[] byteArray, byte[] magicBytes)
+        {
+            if (byteArray.Length < magicBytes.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < magicBytes.Length; i++)
+            {
+                if (byteArray[i] != magicBytes[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
     public string DecryptFile(string encryptedText, string decryptionKey = "OCRAOCRAOCRAOCRA")
