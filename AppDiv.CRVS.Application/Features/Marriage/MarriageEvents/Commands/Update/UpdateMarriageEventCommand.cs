c81f5ce1ed0e24cddb4ab0353006a41857de1e49
t.tsegaye <t.tsegaye@appdiv.com>
2023-08-22 14:16:07 +0300

    fix: update marriage event missing feilds added

--- AppDiv.CRVS.Application/Features/Marriage/MarriageEvents/Commands/Update/UpdateMarriageEventCommand.cs
+++ AppDiv.CRVS.Application/Features/Marriage/MarriageEvents/Commands/Update/UpdateMarriageEventCommand.cs
@@ -19,8 +19,10 @@ namespace AppDiv.CRVS.Application.Features.MarriageEvents.Command.Update
         public Guid MarriageTypeId { get; set; }
         public Guid? ApplicationId { get; set; }
         public AddMarriageApplicationRequest? Application { get; set; }
-
+        public string? BirthCertificateGroomId { get; set; }
+        public string? BirthCertificateBrideId { get; set; }
         public bool HasCamera { get; set; } = false;
+        public bool HasVideo { get; set; } = false;
         public virtual BrideInfoDTO BrideInfo { get; set; }
         public virtual AddEventForMarriageRequest Event { get; set; }
         public virtual ICollection<AddWitnessRequest> Witnesses { get; set; }
