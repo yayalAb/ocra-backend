01ad902ae5cb2f88ece28ff05bea7d9ef187d2c5
t.tsegaye <t.tsegaye@appdiv.com>
2023-08-27 14:38:46 +0300

    fix: marriage application createdDate to string

--- AppDiv.CRVS.Application/Contracts/Request/AddMarriageApplicationRequest.cs
+++ AppDiv.CRVS.Application/Contracts/Request/AddMarriageApplicationRequest.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using AppDiv.CRVS.Application.Contracts.DTOs;
+using AppDiv.CRVS.Utility.Services;
 using Newtonsoft.Json.Linq;
 
 namespace AppDiv.CRVS.Application.Contracts.Request
@@ -11,7 +13,21 @@ namespace AppDiv.CRVS.Application.Contracts.Request
         public BrideInfoDTO BrideInfo { get; set; }
         public GroomInfoDTO GroomInfo { get; set; }
         public Guid CivilRegOfficerId { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public string? CreatedDate { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+        [NotMapped]
+        public string? _CreatedDate
+        {
+            get { return CreatedDate; }
+            set
+            {
+                // this.CreatedDate = value;
+
+                CreatedAt = string.IsNullOrEmpty(CreatedDate) ? null : new CustomDateConverter(CreatedDate).gorgorianDate;
+            }
+        }
+
     }
 }
 
