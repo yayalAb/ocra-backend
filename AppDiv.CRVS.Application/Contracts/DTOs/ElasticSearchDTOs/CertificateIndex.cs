3c589f96c41a0f322d0a7d278b91d4164e8bf556
t.tsegaye <t.tsegaye@appdiv.com>
2023-09-04 18:14:16 +0300

    fix: certificate search  based on address

--- AppDiv.CRVS.Application/Contracts/DTOs/ElasticSearchDTOs/CertificateIndex.cs
+++ AppDiv.CRVS.Application/Contracts/DTOs/ElasticSearchDTOs/CertificateIndex.cs
@@ -1,45 +1,41 @@
 
+using Nest;
+
 namespace AppDiv.CRVS.Application.Contracts.DTOs.ElasticSearchDTOs
 {
-    public class CertificateIndex 
+    public class CertificateIndex
     {
         public Guid Id { get; set; }
         public Guid EventId { get; set; }
         public Guid? NestedEventId { get; set; }
-        public string? EventType {get; set; }
-        public string? CertificateId {get; set; }
-        public string? CertificateSerialNumber {get; set; }
+        public string? EventType { get; set; }
+        public string? CertificateId { get; set; }
+        public string? CertificateSerialNumber { get; set; }
         public string? ContentStr { get; set; }
-        public string? FirstNameOr { get; set; }
-        public string? FirstNameAm { get; set; }
-        public string? MiddleNameOr { get; set; }
-        public string? MiddleNameAm { get; set; }
-        public string? LastNameOr { get; set; }
-        public string? LastNameAm { get; set; }
+        public string? FullNameAm { get; set; }
+        public string? FullNameOr { get; set; }
         public string? NationalId { get; set; }
         public string? PhoneNumber { get; set; }
-        public DateTime? BirthDate {get; set;}
+        public DateTime? BirthDate { get; set; }
         public string? GenderOr { get; set; }
         public string? GenderAm { get; set; }
-        public string? AddressOr { get;set; }
-        public string? AddressAm { get; set;}
-        public string? MotherFirstNameAm {get; set; }
-        public string? MotherFirstNameOr {get; set; }
-        public string? MotherMiddleNameAm {get; set; }
-        public string? MotherMiddleNameOr {get; set; }
-        public string? MotherLastNameAm {get; set; }
-        public string? MotherLastNameOr {get; set; }
-        public string? CivilRegOfficerNameAm {get; set; }
-        public string? CivilRegOfficerNameOr {get; set; }
-        public string? EventAddressAm {get; set; }
-        public string? EventAddressOr {get; set; }
-        public Guid? EventRegisteredAddressId {get; set; }
-        public bool? Status {get;set;}
+        public string? AddressOr { get; set; }
+        public string? AddressAm { get; set; }
+        public string? MotherFullNameAm { get; set; }
+        public string? MotherFullNameOr { get; set; }
+        public string? CivilRegOfficerNameAm { get; set; }
+        public string? CivilRegOfficerNameOr { get; set; }
+        public string? EventAddressAm { get; set; }
+        public string? EventAddressOr { get; set; }
+        public string? EventRegisteredAddressId { get; set; }
+        public string? EventRegisteredAddressAm {get; set; }
+        public string? EventRegisteredAddressOr {get; set; }
+        public bool? Status { get; set; }
 
 
-        
-        
-    
+
+
+
 
 
     }
