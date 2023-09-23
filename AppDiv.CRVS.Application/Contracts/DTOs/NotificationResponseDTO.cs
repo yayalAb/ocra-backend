1e8248074ac798e4a4c40f8bfb5ad84aede8aa6c
t.tsegaye <t.tsegaye@appdiv.com>
2023-09-23 11:56:22 +0300

    fix: approval type on notification res

--- AppDiv.CRVS.Application/Contracts/DTOs/NotificationResponseDTO.cs
+++ AppDiv.CRVS.Application/Contracts/DTOs/NotificationResponseDTO.cs
@@ -1,21 +1,24 @@
 
 namespace AppDiv.CRVS.Application.Contracts.DTOs
 {
-    public class NotificationResponseDTO 
+    public class NotificationResponseDTO
     {
-        public Guid Id {get; set; }
+        public Guid Id { get; set; }
         public string Type { get; set; }
         public string MessageStr { get; set; }
-        public Guid NotificationObjId {get; set; }
+        public Guid NotificationObjId { get; set; }
         public Guid? EventId { get; set; }
-        public string SenderId {get; set;}
+        public string SenderId { get; set; }
         // public bool Seen { get; set; }
-        public Guid? RequestId {get; set;}
+        public Guid? RequestId { get; set; }
         public Guid? GroupId { get; set; }
-        public string? ReceiverId {get; set;}
+        public string? ReceiverId { get; set; }
         public DateTime CreatedAt { get; set; }
-        public string SenderUserName {get; set; }
-        public string SenderFullName {get; set; }
-        public bool hasApproval {get; set;}
+        public string SenderUserName { get; set; }
+        public string SenderFullName { get; set; }
+        public string ApprovalType { get; set; }
+
+        public bool hasApproval { get; set; }
+
     }
 }
